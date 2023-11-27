290ceccff8066bb2a5a0ed4af91b8570e79305a1
Leonardo Lobato <leonardo@skyworkz.nl>
2023-11-27 16:35:36 +0100

    remove unused option

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -8,8 +8,7 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access = true
 
   cluster_addons = {
     aws-ebs-csi-driver = {
